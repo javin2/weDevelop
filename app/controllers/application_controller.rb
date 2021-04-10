8ef9024204a8d9d717bff038f0eaed70b4529a1b
Josh <javin2@uis.edu>
2021-04-10 15:10:04 -0700

    cleaned up routes.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
 	private
 		def after_sign_in_path_for(resource)
-		 developers_index_path
+		 developers_path
 		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
   devise_for :developers
+  resources :notes
+  resources :developers
+  resources :clients
+  resources :projects
   root 'pages#home'
-  get 'notes/index'
-  get 'notes/show'
-  get 'notes/new'
-  get 'notes/edit'
-  get 'developers/index'
-  get 'developers/show'
-  get 'developers/new'
-  get 'developers/edit'
-  get 'clients/index'
-  get 'clients/show'
-  get 'clients/new'
-  get 'clients/edit'
-  get 'projects/index'
-  get 'projects/show'
-  get 'projects/new'
-  get 'projects/edit'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
