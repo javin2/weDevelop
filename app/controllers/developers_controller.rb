323d3bc78807bb38b0e1c1ee6888d5b0efa7f386
Josh <javin2@uis.edu>
2021-04-15 23:07:39 -0700

    added another button to nav/added backend code and links into view pages to show the data

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,6 +1,12 @@
 class DevelopersController < ApplicationController
+	before_action :authenticate_developer!
+
 
   def dashboard
+  	@projects = current_developer.projects
+  end
+
+  def client_details
   	@projects = current_developer.projects
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'clients/dashboard'
   get '/developers/dashboard' => "developers#dashboard", :as => :developer_root
   get '/clients/dashboard' => "clients#dashboard", :as => :client_root
+  get '/developers/client_details'
   devise_for :clients, controllers: { sessions: "clients/sessions", registrations: "clients/registrations" }
   devise_for :developers, controllers: { sessions: "developers/sessions", registrations: "developers/registrations" }
   resources :notes
