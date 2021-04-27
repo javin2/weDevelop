716d0c7e9d5acfad92d0d8e1a92c66b9098af925
Josh <javin2@uis.edu>
2021-04-27 01:36:37 -0700

    modified controllers to work with the nested create forms - successfully saves new 'update' record in db

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,13 +1,33 @@
 class NotesController < ApplicationController
-  def index
-  end
+  
 
   def show
   end
 
   def new
+
   end
 
-  def edit
+  def create
+  	@project = current_developer.projects.find(params[:project_id])
+  	@note = @project.notes.new(note_params)
+  	@note.project = @project 
+  	@note.developer = current_developer
+
+  	respond_to do |format|
+  		if @note.save
+  			format.html {redirect_to @project, notice: 'Your update was created successfully!'}
+  		else
+  			format.html {render @project, status: :unprocessable_entity}
+  		end
+  	end
   end
+
+
+  private
+
+  	def note_params
+  		params.require(:note).permit(:title, :body, :project_id, :developer_id)
+  	end
+
 end
